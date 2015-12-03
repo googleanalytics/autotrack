7a590cff595cabe74256380dd9cd647259b8287e
Philip Walton <philip@philipwalton.com>
2015-12-03 10:07:40 -0800

    Upgrade webdriverio

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,88 +1,72 @@
+// When running on CI, this will be true
+var isSauceLabs = process.env.SAUCE_USERNAME && process.env.SAUCE_ACCESS_KEY;
+
+
+// https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
+var capabilities = [{browserName: 'firefox'}];
+if (isSauceLabs) {
+  capabilities = capabilities.concat([
+    {
+      browserName: 'chrome'
+    },
+    {
+      browserName: 'MicrosoftEdge',
+      platform: 'Windows 10'
+    },
+    {
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    },
+    {
+      browserName: 'internet explorer',
+      platform: 'Windows 8',
+      version: '10'
+    },
+    {
+      browserName: 'internet explorer',
+      platform: 'Windows 8.1',
+      version: '11'
+    },
+    {
+      browserName: 'safari',
+      platform: 'OS X 10.11'
+    },
+    {
+      browserName: 'safari',
+      platform: 'OS X 10.8',
+      version: '6'
+    }
+  ]);
+}
+
+
 exports.config = {
 
-    user: process.env.SAUCE_USERNAME,
-    key:  process.env.SAUCE_ACCESS_KEY,
-    updateJob: true,
+  user: process.env.SAUCE_USERNAME,
+  key:  process.env.SAUCE_ACCESS_KEY,
 
-    /**
-     * specify test files
-     */
-    specs: [
-        './test.js'
-    ],
-    // exclude: [
-    //     'test/spec/multibrowser/**',
-    //     'test/spec/mobile/**'
-    // ],
+  updateJob: true,
 
-    /**
-     * capabilities
-     * https://wiki.saucelabs.com/display/DOCS/Platform+Configurator#/
-     */
-    capabilities: [
-        {
-            browserName: 'chrome'
-        },
-        {
-            browserName: 'firefox'
-        },
-        {
-            browserName: 'MicrosoftEdge',
-            platform: 'Windows 10'
-        },
-        {
-            browserName: 'internet explorer',
-            platform: 'Windows 8.1',
-            version: '11'
-        },
-        {
-            browserName: 'internet explorer',
-            platform: 'Windows 8',
-            version: '10'
-        },
-        {
-            browserName: 'internet explorer',
-            platform: 'Windows 8.1',
-            version: '11'
-        },
-        {
-            browserName: 'safari',
-            platform: 'OS X 10.11'
-        },
-        {
-            browserName: 'safari',
-            platform: 'OS X 10.8',
-            version: '6'
-        }
-    ],
+  specs: [
+    './test.js'
+  ],
+  // exclude: [
+  //     'test/spec/multibrowser/**',
+  //     'test/spec/mobile/**'
+  // ],
 
-    /**
-     * test configurations
-     */
-    logLevel: 'silent',
-    coloredLogs: true,
-    screenshotPath: 'shots',
-    baseUrl: 'http://webdriver.io',
-    waitforTimeout: 60000,
-    framework: 'mocha',
+  capabilities: capabilities,
 
-    reporter: 'spec',
-    reporterOptions: {
-        outputDir: './'
-    },
+  logLevel: 'verbose',
+  coloredLogs: true,
+  baseUrl: 'http://webdriver.io',
+  waitforTimeout: 60000,
 
-    mochaOpts: {
-        ui: 'bdd'
-    },
-
-    /**
-     * hooks
-     */
-    onPrepare: function() {
-        console.log('let\'s go');
-    },
-    onComplete: function() {
-        console.log('that\'s it');
-    }
-
+  framework: 'mocha',
+  reporter: 'spec',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 60000
+  }
 };
