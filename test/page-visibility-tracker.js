53e4fdee87cba6a03b71502112b9ffd1566e8cd8
Philip Walton <philip@philipwalton.com>
2016-03-22 13:21:38 -0700

    Update pageVisibilityTracker tests to load once

--- test/page-visibility-tracker.js
+++ test/page-visibility-tracker.js
@@ -16,7 +16,7 @@
 
 
 var assert = require('assert');
-var get = require('lodash/object/get');
+var ga = require('./analytics');
 var constants = require('../lib/constants');
 
 
@@ -34,22 +34,37 @@ describe('pageVisibilityTracker', function() {
   before(function *() {
     browserCaps = (yield browser.session()).value;
     command = browserCaps.platform.indexOf('OS X') < 0 ? META : CTRL;
+
+    // Loads the autotrack file since no custom HTML is needed.
+    yield browser.url('/test/autotrack.html');
   });
 
+
+  beforeEach(function() {
+    return browser
+        .execute(ga.run, 'create', 'UA-XXXXX-Y', 'auto')
+        .execute(ga.trackHitData);
+  });
+
+
+  afterEach(function () {
+    return browser
+        .execute(ga.clearHitData)
+        .execute(ga.run, 'pageVisibilityTracker:remove')
+        .execute(ga.run, 'remove');
+  });
 
   it('should send events when the visibility state changes', function *() {
 
     if (notSupportedInBrowser()) return;
 
     var hitData = (yield browser
-        .url('/test/page-visibility-tracker.html')
-        .element('body')
-        .keys(command + 't' + command)
-        .element('body')
-        .keys(command + 'w' + command)
-        .execute(getHitData)).value;
+        .execute(ga.run, 'require', 'pageVisibilityTracker')
+        .element('body').keys(command + 't' + command) // Opens a new tab.
+        .element('body').keys(command + 'w' + command) // Closes the new tab.
+        .execute(ga.getHitData)).value;
 
-    assert.equal(hitData.count, 2);
+    assert.equal(hitData.length, 2);
     assert.equal(hitData[0].eventCategory, 'Page Visibility');
     assert.equal(hitData[0].eventAction, 'change');
     assert.equal(hitData[0].eventLabel, 'hidden');
@@ -65,13 +80,17 @@ describe('pageVisibilityTracker', function() {
     if (notSupportedInBrowser()) return;
 
     var hitData = (yield browser
-        .url('/test/page-visibility-tracker-session-timeout.html')
+        .execute(ga.run, 'require', 'pageVisibilityTracker', {
+          sessionTimeout: 1/60
+        })
         .pause(SESSION_TIMEOUT)
-        .element('body')
-        .keys(command + 't' + command)
-        .execute(getHitData)).value;
+        .element('body').keys(command + 't' + command) // Opens a new tab.
+        .execute(ga.getHitData)).value;
 
-    assert.equal(hitData.count, 0);
+    assert.equal(hitData.length, 0);
+
+    // Closes the new tab.
+    yield browser.element('body').keys(command + 'w' + command);
   });
 
 
@@ -81,18 +100,19 @@ describe('pageVisibilityTracker', function() {
     if (notSupportedInBrowser()) return;
 
     var hitData = (yield browser
-        .url('/test/page-visibility-tracker-session-timeout.html')
+        .execute(ga.run, 'require', 'pageVisibilityTracker', {
+          sessionTimeout: 1/60
+        })
         .pause(SESSION_TIMEOUT)
-        .execute(sendEvent)
-        .execute(getHitData)).value;
+        .execute(ga.run, 'send', 'event', 'Uncategorized', 'inactive')
+        .execute(ga.getHitData)).value;
 
     // Expects non-pageview hits queued to be sent after the session has timed
     // out to include a pageview immediately before them.
-    assert.equal(hitData.count, 2);
+    assert.equal(hitData.length, 2);
     assert.equal(hitData[0].hitType, 'pageview');
     assert.equal(hitData[1].eventCategory, 'Uncategorized');
     assert.equal(hitData[1].eventAction, 'inactive');
-
   });
 
 
@@ -102,17 +122,17 @@ describe('pageVisibilityTracker', function() {
     if (notSupportedInBrowser()) return;
 
     var hitData = (yield browser
-        .url('/test/page-visibility-tracker-session-timeout.html')
+        .execute(ga.run, 'require', 'pageVisibilityTracker', {
+          sessionTimeout: 1/60
+        })
         .pause(SESSION_TIMEOUT)
-        .element('body')
-        .keys(command + 't' + command)
-        .element('body')
-        .keys(command + 'w' + command)
-        .execute(getHitData)).value;
+        .element('body').keys(command + 't' + command) // Opens a new tab.
+        .element('body').keys(command + 'w' + command) // Closes the new tab.
+        .execute(ga.getHitData)).value;
 
     // Expects a pageview in lieu of a visible event because the session
     // has timed out.
-    assert.equal(hitData.count, 1);
+    assert.equal(hitData.length, 1);
     assert.equal(hitData[0].hitType, 'pageview');
   });
 
@@ -123,33 +143,34 @@ describe('pageVisibilityTracker', function() {
     if (notSupportedInBrowser()) return;
 
     var hitData = (yield browser
-        .url('/test/page-visibility-tracker-virtual-pageview-fields.html')
+        .execute(ga.run, 'require', 'pageVisibilityTracker', {
+          sessionTimeout: 1/60,
+          virtualPageviewFields: {
+            dimension1: 'pageVisibilityTracker'
+          }
+        })
         .pause(SESSION_TIMEOUT)
-        .execute(sendEvent)
-        .execute(getHitData)).value;
+        .execute(ga.run, 'send', 'event', 'Uncategorized', 'inactive')
+        .execute(ga.getHitData)).value;
 
     // Expects non-pageview hits queued to be sent after the session has timed
     // out to include a pageview immediately before them.
-    assert.equal(hitData.count, 2);
+    assert.equal(hitData.length, 2);
     assert.equal(hitData[0].hitType, 'pageview');
     assert.equal(hitData[0].dimension1, 'pageVisibilityTracker')
     assert.equal(hitData[1].eventCategory, 'Uncategorized');
     assert.equal(hitData[1].eventAction, 'inactive');
 
-    hitData = (yield browser
-        .url('/test/page-visibility-tracker-virtual-pageview-fields.html')
-        .pause(SESSION_TIMEOUT)
-        .element('body')
-        .keys(command + 't' + command)
-        .element('body')
-        .keys(command + 'w' + command)
-        .execute(getHitData)).value;
+    hitData = (yield browser.pause(SESSION_TIMEOUT)
+        .element('body').keys(command + 't' + command) // Opens a new tab.
+        .element('body').keys(command + 'w' + command) // Closes the new tab.
+        .execute(ga.getHitData)).value;
 
     // Expects non-pageview hits queued to be sent after the session has timed
     // out to include a pageview immediately before them.
-    assert.equal(hitData.count, 1);
-    assert.equal(hitData[0].hitType, 'pageview');
-    assert.equal(hitData[0].dimension1, 'pageVisibilityTracker')
+    assert.equal(hitData.length, 3);
+    assert.equal(hitData[2].hitType, 'pageview');
+    assert.equal(hitData[2].dimension1, 'pageVisibilityTracker')
   });
 
 
@@ -158,21 +179,21 @@ describe('pageVisibilityTracker', function() {
     if (notSupportedInBrowser()) return;
 
     var hitData = (yield browser
-        .url('/test/page-visibility-tracker-session-timeout.html')
+        .execute(ga.run, 'require', 'pageVisibilityTracker', {
+          sessionTimeout: 1/60
+        })
         .pause(SESSION_TIMEOUT / 2)
-        .execute(sendEvent)
+        .execute(ga.run, 'send', 'event', 'Uncategorized', 'inactive')
         .pause(SESSION_TIMEOUT / 2)
-        .element('body')
-        .keys(command + 't' + command)
-        .element('body')
-        .keys(command + 'w' + command)
-        .execute(getHitData)).value;
+        .element('body').keys(command + 't' + command) // Opens a new tab.
+        .element('body').keys(command + 'w' + command) // Closes the new tab.
+        .execute(ga.getHitData)).value;
 
-    assert.equal(hitData.count, 3);
+    assert.equal(hitData.length, 3);
     assert.equal(hitData[0].eventCategory, 'Uncategorized');
     assert.equal(hitData[0].eventAction, 'inactive');
 
-    // Since the event above reset the session timeout, opening a new
+    // Since the event above resets the session timeout, opening a new
     // tab will still be considered within the session timeout.
     assert.equal(hitData[1].eventCategory, 'Page Visibility');
     assert.equal(hitData[1].eventAction, 'change');
@@ -186,38 +207,12 @@ describe('pageVisibilityTracker', function() {
   it('should include the &did param with all hits', function() {
 
     return browser
-        .url('/test/page-visibility-tracker.html')
-        .execute(sendPageview)
-        .waitUntil(hitDataMatches([['[0].devId', constants.DEV_ID]]));
+        .execute(ga.run, 'require', 'pageVisibilityTracker')
+        .execute(ga.run, 'send', 'pageview')
+        .waitUntil(ga.hitDataMatches([['[0].devId', constants.DEV_ID]]));
   });
 
 });
-
-
-function sendPageview() {
-  ga('send', 'pageview');
-}
-
-
-function sendEvent() {
-  ga('send', 'event', 'Uncategorized', 'inactive');
-}
-
-
-function getHitData() {
-  return hitData;
-}
-
-
-function hitDataMatches(expected) {
-  return function() {
-    return browser.execute(getHitData).then(function(hitData) {
-      return expected.every(function(item) {
-        return get(hitData.value, item[0]) === item[1];
-      });
-    });
-  };
-}
 
 
 function isFirefox() {
