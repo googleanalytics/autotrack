93e073cb8e7418a509afb87effbc1100085020b7
Philip Walton <philip@philipwalton.com>
2016-04-06 19:37:34 -0700

    Fix Sauce Labs max VMs issue

--- wdio.conf.js
+++ wdio.conf.js
@@ -44,10 +44,17 @@ exports.config = {
   //
   // First, you can define how many instances should be started at the same time. Let's
   // say you have 3 different capabilities (Chrome, Firefox, and Safari) and you have
-  // set maxInstances to 1; wdio will spawn 3 processes. Therefore, if you have 10 spec
-  // files and you set maxInstances to 10, all spec files will get tested at the same time
+  // set maxInstances to 1, wdio will spawn 3 processes. Therefore, if you have 10 spec
+  // files and you set maxInstances to 10; all spec files will get tested at the same time
   // and 30 processes will get spawned. The property handles how many capabilities
   // from the same test should run tests.
+  //
+  //
+  maxInstances: 5,
+  //
+  // If you have trouble getting all important capabilities together, check out the
+  // Sauce Labs platform configurator - a great tool to configure your capabilities:
+  // https://docs.saucelabs.com/reference/platforms-configurator
   //
   capabilities: getCapabilities(),
   //
